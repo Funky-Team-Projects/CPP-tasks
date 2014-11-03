0ffc865c5dbd1902a619ea6b2e3edea8e8fa5df0
Vladislav <Streamline28@mail.ru>
2014-11-03 18:35:44 +0200

    Useless functionality removed!

--- Lab4/Tasks1.cpp
+++ Lab4/Tasks1.cpp
@@ -54,11 +54,6 @@ int task9(){
 	readInt("Please, would you be so kindly to insert the first number>", a);
 	readInt("Please, would you be so kindly to insert the first number>", b);
 	res = mostProblemSolver(a, b, [](int i){return i*i % 10 == 6; });
-	/*for (int i = a; i <= b; i++){
-		if (i*i % 10 == 6) {
-			res += i;
-		}
-	}*/
 	cout << "Drumroll .... Sum is equal " << res << endl;
 	
 	return 0;
