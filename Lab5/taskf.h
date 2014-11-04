b2fa5297bafd7aef37a3a318ab98afcbf9b6c3e3
Streamline27 <Streamline28@mail.ru>
2014-11-04 09:54:32 +0200

    added header, added main

---
+++ Lab5/taskf.h
@@ -0,0 +1,18 @@
+//=====================
+//include guard defined
+#ifndef __TASKF_H__
+#define __TASKF_H__
+//=====================
+//included dependencies
+#include<iostream>
+#include<stdio.h>
+//=====================
+//included functionality
+int task1();
+int task2();
+int task3();
+
+
+
+
+#endif
