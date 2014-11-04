19d94eecbf27be169ed69f4610d4976672e1f25c
v1ac95 <v1ac95@gmail.com>
2014-11-04 09:41:05 +0200

    added 13 task lab4

--- Lab4/Tasks2.cpp
+++ Lab4/Tasks2.cpp
@@ -11,6 +11,28 @@ int task12(){
 }
 
 int task13(){
+	#include <iostream>
+
+	using namespace std;
+
+	void task13()
+{
+	int a, b, sum = 0, z = 0;
+	float sredArif;
+	cout << "Enter A> "; cin >> a;
+	cout << "Enter B> "; cin >> b;
+	for (int i = a; i <= b; i++)
+	if (i % 10 == 7)
+	{
+		sum = sum + i;
+		z++;
+	}
+	if (z != 0)
+		sredArif = sum / z;
+	cout << "Average of " << a << " and " << b << " that end with 7 are " << sredArif << endl;
+	system("pause");
+
+
 	return 0;
 }
 
