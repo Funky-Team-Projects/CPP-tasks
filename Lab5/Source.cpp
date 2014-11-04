0f1c705b80674968bcaad0d9608856fac17ebf68
v1ac95 <v1ac95@gmail.com>
2014-11-04 09:24:39 +0200

    gyugb

--- Lab5/Source.cpp
+++ Lab5/Source.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 void main()
 {
+	#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	int a, b,sum=0,z=0;
+	float sredArif;
+	for (int i = a; i <= b; i++)
+		if (i % 10 == 7)
+		{
+			sum = sum + i;
+			z++;
+		}
+		if (z != 0)
+			sredArif = sum / z;
+	cout << "Average of " << a << " and " << b << " are " << sredArif << endl;
+	system("pause");
+}
 
 	system("pause");
 }
