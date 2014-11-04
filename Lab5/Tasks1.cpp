6f4b7330cb9dc8522af34ccf6684369630c16fda
Streamline27 <Streamline28@mail.ru>
2014-11-04 09:59:54 +0200

    added task api

--- Lab5/Tasks1.cpp
+++ Lab5/Tasks1.cpp
@@ -7,8 +7,25 @@ int task2()
 	int v1;
 	for (int i = 1; i <= 10; i++){
 		v1 = rand() % 100 + 1;
-		cout << v1
+		cout << v1;
 	}
 	system("pause");
 	return 0;
+}
+
+
+int task4(){
+	return 0;
+}
+
+int task5(){
+	return 0;
+}
+
+int task6(){
+	return 0;
+}
+
+int task7(){
+
 }
