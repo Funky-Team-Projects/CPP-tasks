358e76f10324becf86d974aa8bea9dd4489879ed
v1ac95 <v1ac95@gmail.com>
2014-11-04 09:59:50 +0200

    added task3

--- Lab5/Tasks1.cpp
+++ Lab5/Tasks1.cpp
@@ -7,8 +7,12 @@ int task2()
 	int v1;
 	for (int i = 1; i <= 10; i++){
 		v1 = rand() % 100 + 1;
-		cout << v1;
+		cout << v1 << endl;
 	}
 	system("pause");
+	return 0;
+}
+int task3()
+{
 	return 0;
 }
