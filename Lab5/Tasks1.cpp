8fc4a084cca277c026c53dcd2adb317f9107ab7d
v1ac95 <v1ac95@gmail.com>
2014-11-04 11:00:38 +0200

    made 2 task

--- Lab5/Tasks1.cpp
+++ Lab5/Tasks1.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
 
 int task2()
 {
-	int v1,v2,answ,res,mark;
+	int v1=1, v2=2, answ, res, mark=0;
 	srand(time(NULL));
-	for (int i = 1; i <= 10; i++){
-		v1 = rand() % 100 + 1;
-		v2 = rand() % 100 + 1;
-		if (v1>=v2)
-		cout << v1 << " - " << v2 << endl;
-		cin >> answ;
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+
+	for (int i = 1; i <= 10; i++){		
+		while (v1 < v2) {
+			v1 = rand() % 100 + 1;
+			v2 = rand() % 100 + 1;
+		} //цикл генерации случайных чисел с условием что первое больше или равно второму
+		cout << i<< ". " << v1 << " - " << v2 << " = "; cin >> answ;
 		res = v1 - v2;
-		if (answ == res) 
+		if (answ == res)
 			mark++;
-	}
+		v1 = 1;
+		v2 = 2;
+	} // вывод примеров и запись результата
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+
+	if (mark == 10) cout << setw(3) << mark<<" =>  EXELENT" << endl;
+		else if (mark > 7) cout << mark<< " =>  GOOD ENOUGH" << endl;
+				else if (mark > 5) cout << mark<<" =>  AVERAGE" << endl;
+					else cout << mark <<" =>  YOU SUCK" << endl; //подсчет полученых баллов и вывод оценки
 	system("pause");
 	return 0;
 }
@@ -27,6 +42,7 @@ int task3(){
 
 int task4(){
 	double initalRun = 10;
+	return 0;
 }
 int task5(){
 	return 0;
@@ -37,5 +53,5 @@ int task6(){
 }
 
 int task7(){
-
+	return 0;
 }
