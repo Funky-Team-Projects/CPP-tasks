a5f670b7d4bab0db4dfbf29539f4638349b3c3bb
v1ac95 <v1ac95@gmail.com>
2014-11-07 14:22:02 +0200

    made autorestart

--- Lab5/Main.cpp
+++ Lab5/Main.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 int main(){
-	int taskNumber;
-	readInt("Enter task number: ", taskNumber);
-	selectTask(taskNumber);
-	system("pause");
+	char a;
+	do {
+		system("cls");
+		int taskNumber;
+		readInt("Enter task number: ", taskNumber);
+		selectTask(taskNumber);
+		cout << "Want to do anything else ?  y/n	"; cin >> a;
+	} while (a != 'n');
 	return 0;
 }
