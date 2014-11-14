9c7e77f0bfa8408b49940e39f2652ae15511b80c
Nelosh <asakura.jo@gmail.com>
2014-11-14 14:19:50 +0200

    Tasks added

---
+++ Les6/Task1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+
+void task1(){
+	int n;
+	cout << "Please insert natural number > ";
+	cin >> n;
+	
+}
+
+void task3(){
+
+}
+
+void task5(){
+
+}
+
+void task7(){
+
+}
