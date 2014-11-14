00318deb53fc4b09b0c0880731222c045ff26962
Nelosh <asakura.jo@gmail.com>
2014-11-14 14:48:34 +0200

    Commitment

--- Les6/Task1.cpp
+++ Les6/Task1.cpp
@@ -1,13 +1,29 @@
 #include<iostream>
+#include "tfunc.h"
 
 using namespace std;
 
 
 void task1(){
-	int n;
-	cout << "Please insert natural number > ";
-	cin >> n;
-	
+	int n = 0;
+	while (n <= 0) {
+		cout << "Please insert natural number > ";
+		cin >> n;
+	}
+
+	char min = 10, max = -1;
+	char digit;
+	do {
+		digit = n % 10;
+		if (max < digit) max = digit;
+		if (min > digit) min = digit;
+		n = n / 10;
+	} while (n != 0);
+
+	cout << "Max digit is " << (int)max << endl;
+	cout << "Min digit is " << (int)min << endl;
+
+
 }
 
 void task3(){
