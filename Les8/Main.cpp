b1434f0fcb7ab9921dc2e93095921d1aa21309e0
Vladislav <Streamline28@mail.ru>
2015-02-03 23:57:45 +0200

    conio.h added

--- Les8/Main.cpp
+++ Les8/Main.cpp
@@ -8,4 +8,20 @@ int main(){
 	MazeGenerator * mg = new MazeGenerator(25, 25);
 	mg->generatePaths();
 	return 0;
+	/*
+			#include <stdio.h>
+			#include <conio.h>
+
+			using namespace std;
+			void main(){
+			while (true){
+			if (_kbhit()){
+			printf("HIT!\n");
+			_getch();
+		}
+
+	}
+
+}
+	*/
 }
